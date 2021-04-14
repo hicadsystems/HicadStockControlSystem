09c62daac097a60e1f7dfa58d2345a255ebf66d7
thomasfregene <fregzythomas@gmail.com>
2021-04-14 06:03:02 +0100

    Get Action st_stksystemcontroller

--- HicadStockSystem/HicadStockControlSystem/HicadStockSystem/Controllers/St_StkSystemController.cs
+++ HicadStockSystem/HicadStockControlSystem/HicadStockSystem/Controllers/St_StkSystemController.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using HicadStockSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HicadStockSystem.Controllers
 {
-    public class St_StkSystemController
+    [Route("api/[controller]")] //api/st_stksystem
+    [ApiController()]
+    public class St_StkSystemController : ControllerBase
     {
+        private readonly StockControlDBContext _dbContext;
+
+        public St_StkSystemController(StockControlDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStkSystem()
+        {
+            var stkSystem = await _dbContext.St_StkSystems.ToListAsync();
+            return Ok(stkSystem);
+        }
     }
 }
